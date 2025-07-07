7ee4cf3295ba37b7dd90b170d143ec84c99b84b1
sarahzxwy <sarahab703@gmail.com>
2025-07-07 09:39:55 -0300

    feat: routes for sales management functions

--- src/scripts/scripts-back/services/app.js
+++ src/scripts/scripts-back/services/app.js
@@ -5,6 +5,7 @@ import { fileURLToPath } from "url";
 import { createClient, renderClients, getClientById, searchClients, updateClient, deleteClient } from "../controllers/clientController.js"
 import { createProduct, getProductById, getProductsByCategory, renderProducts, searchProducts, updateProduct, deleteProduct } from "../controllers/productController.js";
 import { createStock, renderStock, getStockById, searchStock, updateStock, deleteStock } from "../controllers/stockController.js";
+import { createSale, renderSale, searchSale, getSaleById, updateSale, deleteSale } from "../controllers/saleController.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname  = path.dirname(__filename);
@@ -38,6 +39,13 @@ app.get('/estoque/:idEstoque', getStockById);
 app.get('/buscarEstoque', searchStock);
 app.put("/estoque/editar/:idEstoque", updateStock);
 app.delete('/estoque/:idEstoque', deleteStock);
+
+app.get('/venda', renderSale);
+app.post('/venda', createSale);
+app.get('/venda/:idVenda', getSaleById);
+app.get('/buscarVenda', searchSale);
+app.put("/venda/editar/:idVenda", updateSale);
+app.delete('/venda/:idVenda', deleteSale);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
